3b333dfd9c9bc7afd7c49914ced693c19551d3ab
Balu-mudhiraj <balakrishnasripathi2001@gmail.com>
2026-09-14 23:00:06 +0530

    Second SpringBoot project

---
+++ SecondSB/src/main/java/com/codewithme/www/SecondSbApplication.java
@@ -0,0 +1,13 @@
+package com.codewithme.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSbApplication.class, args);
+	}
+
+}
